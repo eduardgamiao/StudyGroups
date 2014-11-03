234eb2851cb5071a2ad05e79507069da728a6862
alvinwkwang <wang.wk.alvin@gmail.com>
2014-11-02 22:07:30 -1000

    added example of collapsible items for lectures

--- app/controllers/Lectures.java
+++ app/controllers/Lectures.java
@@ -3,12 +3,12 @@ package controllers;
 import models.Course;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.lecture.ViewLecture;
+import views.html.lecture.ListOfLectures;
 
 public class Lectures extends Controller{
   
   public static Result viewLecture(String id) {
     Course course = Course.find().byId(id);
-    return ok(ViewLecture.render(course.getId(), course));
+    return ok(ListOfLectures.render(course.getId(), course));
   }
 }
